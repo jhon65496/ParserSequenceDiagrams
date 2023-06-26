2e9b12edac34708a6aa477c68439a8208768a879
jhon65496 <jhon65496@yandex.ru>
2023-06-26 09:30:33 +0300

    Добавьте файлы проекта.

---
+++ Core/Habra/HabraParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp.Dom.Html;
+
+namespace Parser.Core.Habra
+{
+    class HabraParser : IParser<string[]>
+    {
+        public string[] Parse(IHtmlDocument document)
+        {
+            var list  = new List<string>();
+            // var items = document.QuerySelectorAll("a").Where(item => item.ClassName != null && item.ClassName.Contains("post__title_link"));
+            // var items = document.QuerySelectorAll("a > span").Where(item => item.ClassName != null && item.ClassName.Contains("tm-title tm-title_h2"));
+            var items = document.QuerySelectorAll("div.tm-article-snippet.tm-article-snippet > h2 > a > span");
+
+            foreach (var item in items)
+            {
+                list.Add(item.TextContent);
+            }
+
+            return list.ToArray();
+        }
+    }
+}
